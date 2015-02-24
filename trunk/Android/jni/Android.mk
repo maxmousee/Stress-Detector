ac147d70ee67f41108b5a5730d78e9ab4ec04bf4
Natan Facchin <maxmousee@gmail.com>
2015-02-24 10:30:23 -0300

    Added source libs

--- trunk/Android/jni/Android.mk
+++ trunk/Android/jni/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := vsd
-LOCAL_SRC_FILES := vsd.cpp
+LOCAL_SRC_FILES := BufferManager.cpp
 
 include $(BUILD_SHARED_LIBRARY)
---
+++ trunk/Android/jni/processAudio_terminate.cpp
@@ -0,0 +1,27 @@
+/*
+ * File: processAudio_terminate.c
+ *
+ * MATLAB Coder version            : 2.6
+ * C/C++ source code generated on  : 06-Jun-2014 20:32:45
+ */
+
+/* Include files */
+#include "processAudio.h"
+#include "processAudio_terminate.h"
+
+/* Function Definitions */
+
+/*
+ * Arguments    : void
+ * Return Type  : void
+ */
+void processAudio_terminate(void)
+{
+  /* (no terminate code required) */
+}
+
+/*
+ * File trailer for processAudio_terminate.c
+ *
+ * [EOF]
+ */
