2ac2d4a26c49d41741cb13f99b4088d19262a887
Natan Facchin <maxmousee@gmail.com>
2015-09-29 23:28:49 -0400

    Added install python dependencies script for Debian based Linux

---
+++ trunk/Python/StressDetector/install_py_dependencies_debian.sh
@@ -0,0 +1,8 @@
+apt-get install python-pip;
+apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose;
+cd ~/;
+mkdir repo;
+cd repo;
+git clone https://github.com/maxmousee/Stress-Detector.git -b master
+#need to change this
+#todo finish and start webservice
