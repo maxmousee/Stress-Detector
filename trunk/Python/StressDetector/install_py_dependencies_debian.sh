1d387217a43535031ca7f9af46b8f21c67d1478b
Natan Facchin <maxmousee@gmail.com>
2015-10-13 18:54:28 -0700

    Finished install script for debian based distros

--- trunk/Python/StressDetector/install_py_dependencies_debian.sh
+++ trunk/Python/StressDetector/install_py_dependencies_debian.sh
@@ -1,10 +1,14 @@
-apt-get install python-pip;
-apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose;
-apt-get install git;
-apt-get install curl;
+#on terminal: sudo bash install_py_depedencies_debian.sh
+apt-get install -y python-pip;
+apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose;
+apt-get install -y git;
+apt-get install -y curl;
+easy_install web.py;
 cd ~/;
 mkdir repo;
 cd repo;
 git clone https://github.com/maxmousee/Stress-Detector.git -b master
 #need to change this
-#todo finish and start webservice
+#to run it just go to the folder and type "python StressDetector.py" without the quotes
+#it will run as a http service listening to port 8080
+#to send a test request just run "curl -H "Content-Type: application/json" --data @audiodata.json http://localhost:8080/processAudio"
