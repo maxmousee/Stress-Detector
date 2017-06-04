7febb1bdf91f65cca64e59ea464991e93330d32d
Natan Facchin <maxmousee@gmail.com>
2017-06-04 00:50:34 -0300

    Added input file parsing argument to Python

--- Python/StressDetectorDesktop.py
+++ Python/StressDetectorDesktop.py
@@ -10,58 +10,85 @@ Created on 22 Jan 2014
 from scipy.io import wavfile
 import emd
 import os
+import sys, getopt
 import matplotlib.pyplot as plt
-rate1,dat1 = wavfile.read(os.getcwd() + "/rain_man_driver_8000.wav")
-#print dat1
-#print rate1
-myemd = emd.emd(dat1, extrapolation=None, nimfs=8, shifting_distance=0.2)
-#myemd = EMD.emd(dat1,  extrapolation='mirror', nimfs=5, shifting_distance=0.2)
-print "processing EMD"
-print myemd
-countZeros = 0
-imfCount = len(myemd[0])
-print imfCount
-if imfCount > 3:
-    imfCount = imfCount - 1
-print "current imfcount is {}".format(imfCount)
 
-mydata = []
-for freq in myemd:
-    #print freq[imfCount - 1]
-    mydata.append(freq[imfCount - 1])
+def main(argv):
+    inputfile = ''
 
-for i in xrange(len(mydata)-1):
-    #print mydata[i]
-    if mydata[i] > 0 and mydata[i+1] < 0:
-        print "crossing between indexes {} and {}".format(i, i+1)
-        countZeros = countZeros + 1
-    elif mydata[i] < 0 and mydata[i+1] > 0:
-        print "crossing between indexes {} and {}".format(i, i+1)
-        countZeros = countZeros + 1
-print "total number of elements is {}".format(len(mydata))
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile="])
+    except getopt.GetoptError:
+        print 'python StressDetectorDesktop.py -i <inputfile>'
+        sys.exit(2)
 
-#for audata in mydata:
-    #print audata
+    for opt, arg in opts:
+       if opt == '-h':
+           print 'python StressDetectorDesktop.py -i <inputfile>'
+           sys.exit()
+       elif opt in ("-i", "--ifile"):
+           inputfile = arg
 
-#plt.plot(myemd[imfCount-1])
+    print 'Input file is "', inputfile
+    if not os.path.isfile(os.getcwd() + "/" + inputfile):
+        # file does NOT exist
+        print "File does NOT exist, will exit"
+        sys.exit(2)
 
-#print "number of crossing zeros {}".format(countZeros)
-audiotimelength = len(mydata)/float(rate1)
-print "total audio file length in seconds is {}".format(audiotimelength)
+    rate1,dat1 = wavfile.read(os.getcwd() + "/" + inputfile)
+    #print dat1
+    #print rate1
+    myemd = emd.emd(dat1, extrapolation=None, nimfs=8, shifting_distance=0.2)
+    #myemd = EMD.emd(dat1,  extrapolation='mirror', nimfs=5, shifting_distance=0.2)
+    print "processing EMD"
+    print myemd
+    countZeros = 0
+    imfCount = len(myemd[0])
+    print imfCount
+    if imfCount > 3:
+        imfCount = imfCount - 1
+    print "current imfcount is {}".format(imfCount)
 
-stresstremoravg = countZeros - 1
-#print "countZeros - 1 is {}".format(stresstremoravg)
-stresstremoravg = stresstremoravg/audiotimelength
-print "stress microtremor avg freq is {}".format(stresstremoravg)
-if stresstremoravg > 12:
-    print "subject is under stress"
-elif stresstremoravg < 8:
-    print "subject is under stress"
-else:
-    print "subject is NOT under stress"
+    mydata = []
+    for freq in myemd:
+        #print freq[imfCount - 1]
+        mydata.append(freq[imfCount - 1])
 
-plt.plot(mydata)
-plt.show()
-#plt.plot(myemd)
-#plt.show()
-#get the second to last array. plot only it (penultima)
+    for i in xrange(len(mydata)-1):
+        #print mydata[i]
+        if mydata[i] > 0 and mydata[i+1] < 0:
+            print "crossing between indexes {} and {}".format(i, i+1)
+            countZeros = countZeros + 1
+        elif mydata[i] < 0 and mydata[i+1] > 0:
+            print "crossing between indexes {} and {}".format(i, i+1)
+            countZeros = countZeros + 1
+    print "total number of elements is {}".format(len(mydata))
+
+    #for audata in mydata:
+        #print audata
+
+    #plt.plot(myemd[imfCount-1])
+
+    #print "number of crossing zeros {}".format(countZeros)
+    audiotimelength = len(mydata)/float(rate1)
+    print "total audio file length in seconds is {}".format(audiotimelength)
+
+    stresstremoravg = countZeros - 1
+    #print "countZeros - 1 is {}".format(stresstremoravg)
+    stresstremoravg = stresstremoravg/audiotimelength
+    print "stress microtremor avg freq is {}".format(stresstremoravg)
+    if stresstremoravg > 12:
+        print "subject is under stress"
+    elif stresstremoravg < 8:
+        print "subject is under stress"
+    else:
+        print "subject is NOT under stress"
+
+    plt.plot(mydata)
+    plt.show()
+    #plt.plot(myemd)
+    #plt.show()
+    #get the second to last array. plot only it (penultima)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
