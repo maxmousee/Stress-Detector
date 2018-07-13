2a2f707e81c5b9dacfa49c316aef406b379feddd
Natan Facchin <maxmousee@gmail.com>
2018-07-13 05:14:19 -0400

    Faster and cleaner python integration tests

--- Python/testws.sh
+++ Python/testws.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-python StressDetectorWS.py & sleep 5; curl -H "Content-Type: application/json" --data @audiodata.json http://localhost:8080/processAudio;
+python Python/StressDetectorWS.py & sleep 5; curl -H "Content-Type: application/json" --data @Python/audiodata.json http://localhost:8080/processAudio;
 kill $!;
+cd ..;
