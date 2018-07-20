1e940ec8c4b74560043cd3d848c4f8daaf01c9fb
Natan Facchin <maxmousee@gmail.com>
2018-07-20 22:51:47 +0200

    Added JAVA JNI port

---
+++ Generic/JAVA/bluesteinSetup.h
@@ -0,0 +1,26 @@
+/*
+ * File: bluesteinSetup.h
+ *
+ * MATLAB Coder version            : 4.0
+ * C/C++ source code generated on  : 20-Jul-2018 00:14:01
+ */
+
+#ifndef BLUESTEINSETUP_H
+#define BLUESTEINSETUP_H
+
+/* Include Files */
+#include <stddef.h>
+#include <stdlib.h>
+#include "rtwtypes.h"
+#include "vsd_types.h"
+
+/* Function Declarations */
+extern void bluesteinSetup(creal_T wwc[15999]);
+
+#endif
+
+/*
+ * File trailer for bluesteinSetup.h
+ *
+ * [EOF]
+ */
---
+++ Generic/JAVA/interface/_coder_vsd_info.h
@@ -0,0 +1,25 @@
+/* 
+ * File: _coder_vsd_info.h 
+ *  
+ * MATLAB Coder version            : 4.0 
+ * C/C++ source code generated on  : 20-Jul-2018 00:14:01 
+ */
+
+#ifndef _CODER_VSD_INFO_H
+#define _CODER_VSD_INFO_H
+/* Include Files */ 
+#include "tmwtypes.h"
+#include "mex.h"
+#include "emlrt.h"
+
+
+/* Function Declarations */ 
+extern const mxArray *emlrtMexFcnResolvedFunctionsInfo(void);
+MEXFUNCTION_LINKAGE mxArray *emlrtMexFcnProperties(void);
+
+#endif
+/* 
+ * File trailer for _coder_vsd_info.h 
+ *  
+ * [EOF] 
+ */
