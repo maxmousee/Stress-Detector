fc3f6bc2391ce2fedcf482bb7e1c67e7d7228f4b
Natan Facchin <maxmousee@gmail.com>
2018-07-25 04:55:37 -0400

    Split Java Mac and Java Linux code

---
+++ Generic/C/Linux/vsd_data.c
@@ -0,0 +1,20 @@
+/*
+ * File: vsd_data.c
+ *
+ * MATLAB Coder version            : 4.0
+ * C/C++ source code generated on  : 24-Jul-2018 00:37:24
+ */
+
+/* Include Files */
+#include "rt_nonfinite.h"
+#include "vsd.h"
+#include "vsd_data.h"
+
+/* Variable Definitions */
+omp_nest_lock_t emlrtNestLockGlobal;
+
+/*
+ * File trailer for vsd_data.c
+ *
+ * [EOF]
+ */
---
+++ Generic/C/Linux/vsd_initialize.h
@@ -0,0 +1,27 @@
+/*
+ * File: vsd_initialize.h
+ *
+ * MATLAB Coder version            : 4.0
+ * C/C++ source code generated on  : 24-Jul-2018 00:37:24
+ */
+
+#ifndef VSD_INITIALIZE_H
+#define VSD_INITIALIZE_H
+
+/* Include Files */
+#include <stddef.h>
+#include <stdlib.h>
+#include "rtwtypes.h"
+#include "omp.h"
+#include "vsd_types.h"
+
+/* Function Declarations */
+extern void vsd_initialize(void);
+
+#endif
+
+/*
+ * File trailer for vsd_initialize.h
+ *
+ * [EOF]
+ */
