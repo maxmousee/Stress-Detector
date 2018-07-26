407e3bc5359ca21bcce9ac11233bb8e8e0fc4ae8
Natan Facchin <maxmousee@gmail.com>
2018-07-26 09:37:10 -0400

    Unified Linux and Mac code

--- Generic/JAVA_C/Mac/VSD.java
+++
@@ -1,6 +0,0 @@
-public class VSD {
-    /* loading the C DLL */
-    static { System.loadLibrary("MATLAB_JNI"); } 
-    /* functions prototypes */
-    public static native double vsd(double[] input_data, int input_size);
-}
--- Generic/JAVA_C/vsd.h
+++ Generic/JAVA_C/vsd.h
@@ -2,7 +2,7 @@
  * File: vsd.h
  *
  * MATLAB Coder version            : 4.0
- * C/C++ source code generated on  : 24-Jul-2018 00:37:24
+ * C/C++ source code generated on  : 20-Jul-2018 23:11:40
  */
 
 #ifndef VSD_H
@@ -12,16 +12,9 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "rtwtypes.h"
-#include "omp.h"
 #include "vsd_types.h"
 
 /* Function Declarations */
-extern double vsd(const double input_data[], const int input_size[1]);
+extern double vsd(const double input_data[], const int input_size);
 
 #endif
-
-/*
- * File trailer for vsd.h
- *
- * [EOF]
- */
