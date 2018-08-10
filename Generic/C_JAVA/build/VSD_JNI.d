ef52d628c3ec6d4ad8da2793bd6d88b686192eed
Natan Facchin <maxmousee@gmail.com>
2018-08-10 16:55:20 +0200

    Removed build dir

--- Generic/C_JAVA/build/VSD_JNI.d
+++
@@ -1,4 +0,0 @@
-build/VSD_JNI.o: VSD_JNI.cpp VSD_JNI.h \
-  /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/include/jni.h \
-  /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/include/darwin/jni_md.h \
-  vsd.h rtwtypes.h vsd_types.h
