d22568fedd66d1945ee0bffc44502598d066fce9
Natan Facchin <maxmousee@gmail.com>
2018-08-10 22:10:23 +0200

    Fixed all bullshit, never again

--- Generic/C_JAVA/VSDJNI.cpp
+++ Generic/C_JAVA/VSDJNI.cpp
@@ -5,11 +5,8 @@ JNIEXPORT jdouble JNICALL Java_VSDJNI_processAudio(JNIEnv* env, jobject obj, jdo
 {
       jdouble* body = env->GetDoubleArrayElements(array, 0);
       jsize len = env->GetArrayLength(array);
-      printf("1\n");
       jdouble* inputAudioBuffer = env->GetDoubleArrayElements(array, 0);
-      printf("3\n");
       double stressFrequency = vsd(inputAudioBuffer, 8000);
-      printf("4\n");
-      //env->ReleaseDoubleArrayElements(array, body, 0);
+      env->ReleaseDoubleArrayElements(array, body, 0);
       return stressFrequency;
 }
