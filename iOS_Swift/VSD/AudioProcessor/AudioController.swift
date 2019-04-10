115c398f73f10a199abd92e81e19454abd67d8fa
Natan Facchin <maxmousee@gmail.com>
2019-04-10 16:24:05 +0200

    Added public utility classes

---
+++ iOS_Swift/VSD/AudioProcessor/AudioController.swift
@@ -0,0 +1,13 @@
+//
+//  AudioController.swift
+//  VSD
+//
+//  Created by Natan Facchin on 4/10/19.
+//  Copyright © 2019 Natan Facchin. All rights reserved.
+//
+
+import Foundation
+
+class AudioController {
+    
+}
