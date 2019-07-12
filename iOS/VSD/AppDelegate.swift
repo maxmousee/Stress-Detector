adf0afb3098447177715cfd59ef65f374bb2c268
Natan Facchin <maxmousee@gmail.com>
2019-07-12 19:21:18 +0200

    Fixed app not reading mic when app comes back from background

--- iOS/VSD/AppDelegate.swift
+++ iOS/VSD/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        audioController.startRecording()
         return true
     }
 
@@ -38,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        audioController.startRecording()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
